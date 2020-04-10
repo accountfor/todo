2be48af47c25f5ef936fae7c2cb1dce13c515d22
accountfor <eduardomourafilhoefolha@gmail.com>
2020-04-10 05:03:20 -0300

    create view to edit task

--- tasks/views.py
+++ tasks/views.py
@@ -28,9 +28,20 @@ def newTask(request):
         return render(request, 'tasks/addtask.html', {'form': form})
 
 
-def helloworld(request):
-    return HttpResponse('Hello World')
-
-
-def yourName(request, name):
-    return render(request, 'tasks/yourname.html', {'name': name})
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+    
+    if (request.method == 'POST'):
+        form = TaskForm(request.POST, instance=task)
+        
+        if(form.is_valid()):
+            
+            task.save()
+            return redirect('/')
+        else:
+            
+            return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+        
+    else:
+        return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
