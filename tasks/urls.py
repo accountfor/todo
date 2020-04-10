96aa62b0861286e05f853874e46fd3ff9343075f
accountfor <eduardomourafilhoefolha@gmail.com>
2020-04-10 04:23:52 -0300

    List tasks from database

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloworld', views.helloworld),
-    path('', views.taskList),
-    path('yourname/<str:name>', views.yourName, name='your-name'),
+    path('', views.taskList, name="task-list"),
+    path('task/<int:id>', views.taskView, name="task-view"),
 ]
